8bd4331a7c3414f3f14902b03d65e41e829bc220
Daniel Lisko <djlisko01@gmail.com>
2023-01-26 15:29:33 -0800

    added svg directly to header section

--- src/components/GraphHeader/GraphHeader.js
+++ src/components/GraphHeader/GraphHeader.js
@@ -6,7 +6,19 @@ const GraphHeader = () => {
         <p>$921.48</p>
       </div>
       <div className="vertical-align" style={{ marginRight: "28px" }}>
-        <img src={"/images/logo.svg"} height={"40px"} alt="logo"></img>
+        <div height={"40px"}>
+          <svg
+            width="72"
+            height="48"
+            viewBox="0 0 72 48"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g fill="none" fill-rule="evenodd">
+              <circle fill="#382314" cx="48" cy="24" r="24" />
+              <circle stroke="#FFF" stroke-width="2" cx="24" cy="24" r="23" />
+            </g>
+          </svg>
+        </div>
       </div>
     </div>
   );
